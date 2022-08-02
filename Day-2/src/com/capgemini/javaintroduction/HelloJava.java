d96d62ad8b13b9dbd869ba515ca3c59d0e975bbe
91840 <91840@Gaurav>
2022-08-02 15:44:09 +0530

    Second Commit

---
+++ Day-2/src/com/capgemini/javaintroduction/HelloJava.java
@@ -0,0 +1,13 @@
+package com.capgemini.javaintroduction;
+//program for byte datatype
+public class HelloJava {
+
+	public static void main(String[] args) {
+		byte a=127;
+				//a=129
+		//it will give you an error because of byte range
+				System.out.println(a);
+
+	}
+
+}
