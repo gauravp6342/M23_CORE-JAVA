e2ab43a7db59c6d67e34b49a013e5c68c8cf4e2c
91840 <91840@Gaurav>
2022-08-02 16:49:07 +0530

    Second Commit

--- Day-2/src/com/capgemini/javaintroduction/CharProgram.java
+++ Day-2/src/com/capgemini/javaintroduction/CharProgram.java
@@ -1,11 +1,19 @@
 package com.capgemini.javaintroduction;
+
+import java.util.Scanner;
+
 //package on character input
 public class CharProgram {
 
 	public static void main(String[] args) {
 		//compile-time input
-		char x='a';
-		System.out.println("The character is :"+x);
+		//char x='a';
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value of x: ");
+		
+		//run time input
+		char x=s.next().charAt(0);
+		System.out.println("The Character is : "+x);
 
 	}
 
