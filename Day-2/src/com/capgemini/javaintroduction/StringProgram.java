5fed0e29c89093838ac34734e93ef2c93b83dc27
91840 <91840@Gaurav>
2022-08-02 17:01:24 +0530

    Second Commit

---
+++ Day-2/src/com/capgemini/javaintroduction/StringProgram.java
@@ -0,0 +1,14 @@
+package com.capgemini.javaintroduction;
+
+import java.util.Scanner;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		System.out.println(str);
+		
+	}
+
+}
