07b0b5862977252638cc49d44153ac500f9880ce
91840 <91840@Gaurav>
2022-08-02 16:33:25 +0530

    Second Commit

---
+++ Day-2/src/com/capgemini/javaintroduction/IntegerCompileTimeInput.java
@@ -0,0 +1,11 @@
+package com.capgemini.javaintroduction;
+//program on Integer data-type
+public class IntegerCompileTimeInput {
+
+	public static void main(String[] args) {
+		int a=148;
+		System.out.println(a);
+
+	}
+
+}
---
+++ Day-2/src/com/capgemini/javaintroduction/IntegerRunTimeInput.java
@@ -0,0 +1,15 @@
+package com.capgemini.javaintroduction;
+//program on integer=runtime input
+import java.util.Scanner;
+
+public class IntegerRunTimeInput {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		System.out.println(x);
+		
+
+	}
+
+}
