c90340b822c4ee1f7e51d96ca5b235f8fe1b006f
91840 <91840@Gaurav>
2022-08-02 16:44:03 +0530

    Second Commit

---
+++ Day-2/src/com/capgemini/javaintroduction/CharProgram.java
@@ -0,0 +1,12 @@
+package com.capgemini.javaintroduction;
+//package on character input
+public class CharProgram {
+
+	public static void main(String[] args) {
+		//compile-time input
+		char x='a';
+		System.out.println("The character is :"+x);
+
+	}
+
+}
--- Day-2/src/com/capgemini/javaintroduction/IntegerCompileTimeInput.java
+++ Day-2/src/com/capgemini/javaintroduction/IntegerCompileTimeInput.java
@@ -3,8 +3,13 @@ package com.capgemini.javaintroduction;
 public class IntegerCompileTimeInput {
 
 	public static void main(String[] args) {
+		
+		//variable initilization
 		int a=148;
 		System.out.println(a);
+		//"println" is used to print the output in the next line
+		System.out.println("Hey Guys!");
+		System.out.println("Hey folks!");
 
 	}
 
