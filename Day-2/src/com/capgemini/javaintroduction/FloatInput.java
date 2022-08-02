ef8a0dadd56dbc18f3bb56b6a610d014f6f05d6c
91840 <91840@Gaurav>
2022-08-02 18:48:12 +0530

    First Commit

--- Day-2/src/com/capgemini/javaintroduction/FloatInput.java
+++ Day-2/src/com/capgemini/javaintroduction/FloatInput.java
@@ -6,8 +6,8 @@ public class FloatInput {
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
-		float a=in.nextFloat();
-		System.out.println(a);
+		float flt=in.nextFloat();
+		System.out.println(flt);
 
 	}
 
