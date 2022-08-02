2c7b072593ee0a19a819a7c171198aceff1650ba
91840 <91840@Gaurav>
2022-08-02 12:49:31 +0530

    Second Commit

---
+++ Myself-1_Practical and Execution/src/com/practice/exe/MyPracticalExecution.java
@@ -0,0 +1,12 @@
+package com.practice.exe;
+//demo for to connect eclipse with github
+public class MyPracticalExecution {
+
+	public static void main(String[] args) {
+		
+		//print the line
+		System.out.println("Welcome To My Practical Execution");
+
+	}
+
+}
