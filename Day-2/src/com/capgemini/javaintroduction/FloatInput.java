6036f55180a10251d524a0304aa93c840b824954
91840 <91840@Gaurav>
2022-08-02 19:04:12 +0530

    Second Commit

---
+++ Day-2/src/com/capgemini/javaintroduction/FloatInput.java
@@ -0,0 +1,13 @@
+package com.capgemini.javaintroduction;
+
+import java.util.Scanner;
+
+public class FloatInput {
+
+	public static void main(String[] args) {
+		Scanner gp=new Scanner(System.in);
+		float flt=gp.nextFloat();
+		System.out.println(flt);
+	}
+
+}
