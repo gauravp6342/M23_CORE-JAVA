a78f54578f915ec5addf6fea9de15a83bb4ce1c8
91840 <91840@Gaurav>
2022-08-04 15:42:47 +0530

    Second Commit

---
+++ Day-4_Packages,Encapsulation,Access Specifier/src/com/capgemini/package1/Package.java
@@ -0,0 +1,14 @@
+package com.capgemini.package1;
+
+public class Package 
+{
+
+	public int roll_no;
+	public String name;
+	public void display(int roll_no,String name)
+	{
+		System.out.println("The roll no is: "+roll_no+" and Name is: "+name);
+
+	}
+
+}
---
+++ Day-4_Packages,Encapsulation,Access Specifier/src/com/capgemini/package2/DemoOnPackage.java
@@ -0,0 +1,13 @@
+package com.capgemini.package2;
+//import com.capgemini.package1.*;
+import com.capgemini.package1.Package;
+//import com.capgemini.package1.Package;
+public class DemoOnPackage {
+
+	public static void main(String[] args) {
+		Package p=new Package();
+		p.display(26,"Swift");
+
+	}
+
+}
