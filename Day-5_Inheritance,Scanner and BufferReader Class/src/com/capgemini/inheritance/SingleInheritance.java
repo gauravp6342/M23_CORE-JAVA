8677ec044ec52cad1225f8d42c5e58fdeb184234
91840 <91840@Gaurav>
2022-08-05 15:48:56 +0530

    First Commit

---
+++ Day-5_Inheritance,Scanner and BufferReader Class/src/com/capgemini/inheritance/SingleInheritance.java
@@ -0,0 +1,32 @@
+package com.capgemini.inheritance;
+//Program on Single  Inheritance
+class Parent
+{
+	protected int roll_no;
+	void print()
+	{
+		System.out.println("Hey Guys! What's going on?");
+		
+	}
+	
+}
+class Child extends Parent
+{
+	void accept()
+	{
+		System.out.println("Roll_no is "+roll_no);
+	}
+	
+}
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.roll_no=99;
+		c.accept();
+		c.print();
+		
+
+	}
+
+}
