8e27c23fd12fc1abdc231539ed9b8e8ad4d3d490
91840 <91840@Gaurav>
2022-08-05 15:36:47 +0530

    Second Commit

--- Day-4_Packages,Encapsulation,Access Specifier/src/com/capgemini/package4/DemoPublic.java
+++ Day-4_Packages,Encapsulation,Access Specifier/src/com/capgemini/package4/DemoPublic.java
@@ -4,9 +4,9 @@ public class DemoPublic
 {
 	public int a,b;
 	
-	public void dislpay(int a1,int b1)
+	public void dislpay(int a,int b)
 	{
-		System.out.println("The addition is: "+(a1+b1));
+		System.out.println("The addition is: "+(a+b));
 	
 		
 	}
