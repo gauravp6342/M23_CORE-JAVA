bc684b101f807c6d48f47dfb4b56a8b00f1d45fc
91840 <91840@Gaurav>
2022-08-05 15:52:18 +0530

    First Commit

---
+++ Day-1_Git and Githubb/src/com/capgemini/github/EclipseWithGitHub.java
@@ -0,0 +1,16 @@
+package com.capgemini.github;
+//demo for to connect eclipse with github
+public class EclipseWithGitHub {
+
+	public static void main(String[] args) {
+		
+	//print the line
+			System.out.println("Welcome to Capgemini C2TC Program");
+			System.out.println("Java 1st Program");
+			
+	}
+
+}
+
+
+		
